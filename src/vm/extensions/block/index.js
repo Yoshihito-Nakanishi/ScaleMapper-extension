8dd6c25a1e2aacbbb38c56e49a63661c40a36218
Yoshihito Nakanishi <info@yoshihito-nakanishi.com>
2022-06-25 02:12:49 +0900

    fixed cc bug, added scales

--- src/vm/extensions/block/index.js
+++ src/vm/extensions/block/index.js
@@ -50,8 +50,22 @@ const EXTENSION_ID = 'scaleMapper';
 let extensionURL = 'https://Yoshihito-Nakanishi.github.io/ScaleMapper-extension/dist/scaleMapper.mjs';
 
 
-let DorianTable = [
-    0, 2, 3, 5, 7, 9, 10,
+var IonianTable = [
+    0,  2,  4,  5,  7,  9,  11,
+    12, 14, 16, 17, 19, 21, 23,
+    24, 26, 28, 29, 31, 33, 35,
+    36, 38, 40, 41, 43, 45, 47,
+    48, 50, 52, 53, 55, 57, 59,
+    60, 62, 64, 65, 67, 69, 71,
+    72, 74, 76, 77, 79, 81, 83,
+    84, 86, 88, 89, 91, 93, 95,
+    96, 98, 100, 101, 103, 105, 107,
+    108, 110, 112, 113, 115, 117, 119,
+    120, 122, 124, 125, 127
+];
+
+var DorianTable = [
+    0,  2,  3,  5,  7,  9,  10,
     12, 14, 15, 17, 19, 21, 22,
     24, 26, 27, 29, 31, 33, 34,
     36, 38, 39, 41, 43, 45, 46,
@@ -64,22 +78,135 @@ let DorianTable = [
     120, 122, 123, 125, 127
 ];
 
-let IonianTable = [
-    0, 2, 4, 5, 7, 9, 11,
-    12, 14, 16, 17, 19, 21, 23,
-    24, 26, 28, 29, 31, 33, 35,
-    36, 38, 40, 41, 43, 45, 47,
-    48, 50, 52, 53, 55, 57, 59,
-    60, 62, 64, 65, 67, 69, 71,
+var PhrygianTable = [
+    0,  1,  3,  5,  7,  8,  10,
+    12, 13, 15, 17, 19, 20, 22,
+    24, 25, 27, 29, 31, 32, 34,
+    36, 37, 39, 41, 43, 44, 46,
+    48, 49, 51, 53, 55, 56, 58,
+    60, 61, 63, 65, 67, 68, 70,
+    72, 73, 75, 77, 79, 80, 82,
+    84, 85, 87, 89, 91, 92, 94,
+    96, 97, 99, 101, 103, 104, 106,
+    108, 109, 111, 113, 115, 116, 118,
+    120, 121, 123, 125, 127
+];
+
+
+var LydianTable = [
+    0,  2,  4,  6,  7,  9,  11,
+    12, 14, 16, 18, 19, 21, 23,
+    24, 26, 28, 30, 31, 33, 35,
+    36, 38, 40, 42, 43, 45, 47,
+    48, 50, 52, 54, 55, 57, 59,
+    60, 62, 64, 66, 67, 69, 71,
+    72, 74, 76, 78, 79, 81, 83,
+    84, 86, 88, 90, 91, 93, 95,
+    96, 98, 100, 102, 103, 105, 107,
+    108, 110, 112, 114, 115, 117, 119,
+    120, 122, 124, 126, 127
+];
+
+
+var MixolydianTable = [
+    0,  2,  4,  5,  7,  9,  10,
+    12, 14, 16, 17, 19, 21, 22,
+    24, 26, 28, 29, 31, 33, 34,
+    36, 38, 40, 41, 43, 45, 46,
+    48, 50, 52, 53, 55, 57, 58,
+    60, 62, 64, 65, 67, 69, 70,
     72, 74, 76, 77, 79, 81, 83,
     84, 86, 88, 89, 91, 93, 95,
     96, 98, 100, 101, 103, 105, 107,
     108, 110, 112, 113, 115, 117, 119,
     120, 122, 124, 125, 127
-]
+];
 
-let minPentaTable = [
-    0, 3, 5, 7, 10,
+
+var AeolianTable = [
+    0,  2,  3,  5,  7,  8,  10,
+    12, 14, 15, 17, 19, 20, 22,
+    24, 26, 27, 29, 31, 32, 34,
+    36, 38, 39, 41, 43, 44, 46,
+    48, 50, 51, 53, 55, 56, 58,
+    60, 62, 63, 65, 67, 68, 70,
+    72, 74, 75, 77, 79, 80, 82,
+    84, 86, 87, 89, 91, 92, 94,
+    96, 98, 99, 101, 103, 104, 106,
+    108, 110, 111, 113, 115, 116, 118,
+    120, 122, 123, 125, 127
+];
+
+
+var LocrianTable = [
+    0,  1,  3,  5,  6,  8,  10,
+    12, 13, 15, 17, 18, 20, 22,
+    24, 25, 27, 29, 30, 32, 34,
+    36, 37, 39, 41, 42, 44, 46,
+    48, 49, 51, 53, 54, 56, 58,
+    60, 61, 63, 65, 66, 68, 70,
+    72, 73, 75, 77, 78, 80, 82,
+    84, 85, 87, 89, 90, 92, 94,
+    96, 97, 99, 101, 102, 104, 106,
+    108, 109, 111, 113, 115, 117, 119,
+    120, 121, 123, 125, 127
+];
+
+
+var WholetoneTable = [
+    0,  2,  4,  6,  8,  10,
+    12, 14, 16, 18, 20, 22,
+    24, 26, 28, 30, 32, 34,
+    36, 38, 40, 42, 44, 46,
+    48, 50, 52, 54, 56, 58,
+    60, 62, 64, 66, 68, 70,
+    72, 74, 76, 78, 80, 82,
+    84, 86, 88, 90, 92, 94,
+    96, 98, 100, 102, 104, 106,
+    108, 110, 112, 114, 116, 118,
+    120, 122, 124, 126
+];
+
+
+var octaveTable = [
+    0,  12, 24, 36, 48,
+    60, 72, 84, 96, 108,
+    120
+];
+
+
+var Octatonic2_1Table = [
+    0,  2,  3,  5,  6,  8,  9, 11,
+    12, 14, 15, 17, 18, 20, 21, 23,
+    24, 26, 27, 29, 30, 32, 33, 35,
+    36, 38, 39, 41, 42, 44, 45, 47,
+    48, 50, 51, 53, 54, 56, 57, 59,
+    60, 62, 63, 65, 66, 68, 69, 71,
+    72, 74, 75, 77, 78, 80, 81, 83,
+    84, 86, 87, 89, 90, 92, 93, 95,
+    96, 98, 99, 101, 102, 104, 105, 107,
+    108, 110, 111, 113, 114, 116, 117, 119,
+    120, 122, 123, 125, 127
+];
+
+
+var Octatonic1_2Table = [
+    0,  1,  3,  4,  6,  7,  9,  10,
+    12, 13, 15, 16, 18, 19, 21, 22,
+    24, 25, 27, 28, 30, 31, 33, 34,
+    36, 37, 39, 40, 42, 43, 45, 46,
+    48, 49, 51, 52, 54, 55, 57, 58,
+    60, 61, 63, 64, 66, 67, 69, 70,
+    72, 73, 75, 76, 78, 79, 81, 82,
+    84, 85, 87, 88, 90, 91, 93, 94,
+    96, 97, 99, 100, 102, 103, 105, 106,
+    108, 109, 111, 113, 115, 116, 118, 119,
+    120, 121, 123, 125, 127
+];
+
+
+var minPentaTable = [
+    0,  3,  5,  7,  10,
     12, 15, 17, 19, 22,
     24, 27, 29, 31, 34,
     36, 39, 41, 43, 46,
@@ -90,10 +217,10 @@ let minPentaTable = [
     96, 99, 101, 103, 106,
     108, 111, 113, 115, 118,
     120, 123, 125, 127
-]
+];
 
-let majPentaTable = [
-    0, 2, 4, 7, 9,
+var majPentaTable = [
+    0,  2,  4,  7,  9,
     12, 14, 16, 19, 21,
     24, 26, 28, 31, 33,
     36, 38, 40, 43, 45,
@@ -104,7 +231,68 @@ let majPentaTable = [
     96, 98, 100, 103, 105,
     108, 110, 112, 115, 117,
     120, 122, 124, 127
-]
+];
+
+
+var maj3rdTable = [
+    0,  4,  8,
+    12, 16, 20,
+    24, 28, 32,
+    36, 40, 44,
+    48, 52, 56,
+    60, 64, 68,
+    72, 76, 80,
+    84, 88, 92,
+    96, 100, 104,
+    108, 112, 116,
+    120, 124
+];
+
+
+var min3rdTable = [
+    0,  3,  6,  9,
+    12, 15, 18, 21,
+    24, 27, 30, 33,
+    36, 39, 42, 45,
+    48, 51, 54, 57,
+    60, 63, 66, 69,
+    72, 75, 78, 81,
+    84, 87, 90, 93,
+    96, 99, 102, 105,
+    108, 111, 114, 117,
+    120, 123, 126
+];
+
+
+var RyukyuTable = [
+    0,  4,  5,  7,  11,
+    12, 16, 17, 19, 23,
+    24, 28, 29, 31, 35,
+    36, 40, 41, 43, 47,
+    48, 52, 53, 55, 59,
+    60, 64, 65, 67, 71,
+    72, 76, 77, 79, 83,
+    84, 88, 89, 91, 95,
+    96, 100, 101, 103, 107,
+    108, 112, 113, 115, 119,
+    120, 124, 125, 127
+];
+
+
+var ChromaTable = [
+    0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11,
+    12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+    24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+    36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+    48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+    72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
+    84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+    96, 97, 98, 99, 100, 101, 102,103,104,105,106,107,
+    108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+    120, 121, 122, 123, 124, 125, 126, 127
+];
+
 
 let scaleValue = 0;
 
@@ -167,25 +355,20 @@ function midiConnect() {
     const msgOn = [NOTE_ON, pitch, velocity];
     const msgOff = [NOTE_OFF, pitch, velocity];
     
-      if (pitch > 0) {
+      if (pitch > -1  && value < 128) {
           // note on
           device.send(msgOn);
 
           // note off
           const noteOff = function(){
             device.send(msgOff);
-            console.log("noteoff:" + msgOff);
+            console.log("NoteOff:" + msgOff);
           };
 
-          console.log("noteon:" + msgOn);
+          console.log("NoteOn:" + msgOn);
           setTimeout(noteOff, duration);
 
-      } else {
-
-        device.send(msgOff);
-        console.log("noteoff:" + msgOff);
-
-      }
+      } 
 
   }
 
@@ -196,10 +379,12 @@ function midiConnect() {
     const msgCC = [CC, 0, value];
 
     // First send the note on;
-      if (value > 0) {
+    if(value > -1 && value < 128){
 
-          device.send(msgCC);
-      }
+        device.send(msgCC);
+        console.log("ControlChange:" + msgCC);
+    }
+
   }
 
 /**
@@ -466,51 +651,223 @@ class ExtensionBlocks {
                 value: '1'
             },
             {
-                text: `majorPentatonic`,
+                text: `Phrygian`,
                 value: '2'
             },
             {
-                text: `minorPentatonic`,
+                text: `Lydian`,
                 value: '3'
+            },
+            {
+                text: `Mixolydian`,
+                value: '4'
+            },
+            {
+                text: `Aeolian`,
+                value: '5'
+            },
+            {
+                text: `Locrian`,
+                value: '6'
+            },
+            {
+                text: `Wholetone`,
+                value: '7'
+            },
+            {
+                text: `octave`,
+                value: '8'
+            },
+            {
+                text: `Octatonic2_1`,
+                value: '9'
+            },
+            {
+                text: `Octatonic1_2`,
+                value: '10'
+            },
+            {
+                text: `majPentatonic`,
+                value: '11'
+            },
+            {
+                text: `minPentatonic`,
+                value: '12'
+            },
+            {
+                text: `maj3rd`,
+                value: '13'
+            },
+            {
+                text: `min3rdTable`,
+                value: '14'
+            },
+            {
+                text: `Ryukyu`,
+                value: '15'
+            },
+            {
+                text: `Chromatic`,
+                value: '16'
             }
         ];
     }
 
     scaler(args) {
         
-        var result = 0;
-        var midiNote = 0;
+        var rawValue =  args.data;
+        var midi_output = 0;
+        var data = 0;
 
-        if(args.data > 0 && args.data < 128){
-            midiNote = args.data; 
-        } else {
-            midiNote = 0;
+        if(rawValue > 127){
+            data = 127
+        } else if (rawValue < 0){
+            data = 0
+        } else if (rawValue < 128 && rawValue > 0){
+            data = rawValue
         }
 
         switch (args.scale) {
             case '0':
-                var value = (midiNote) / (127 / 75);
-                result = IonianTable[Math.round(value)];
+                var value = data / (127/75);
+                if(value > 74){
+                    value = 74;
+                }
+                midi_output = IonianTable[Math.round(value)];
                 break;
-
+                
             case '1':
-                var value = (midiNote) / (127 / 75);
-                result = DorianTable[Math.round(value)];
+                var value = data / (127/75);
+                if(value > 74){
+                    value = 74;
+                }
+                midi_output = DorianTable[Math.round(value)];
                 break;
 
             case '2':
-                var value = (midiNote) / (127 / 54);
-                result = majPentaTable[Math.round(value)];
+                var value = data / (127/75);
+                if(value > 74){
+                    value = 74;
+                }
+                midi_output = PhrygianTable[Math.round(value)];
                 break;
+
             case '3':
-                var value = (midiNote) / (127 / 54);
-                result = minPentaTable[Math.round(value)];
+                var value = data / (127/75);
+                if(value > 74){
+                    value = 74;
+                }
+                midi_output = LydianTable[Math.round(value)];
                 break;
-            default: 
+
+            case '4':
+                var value = data / (127/75);
+                if(value > 74){
+                    value = 74;
+                }
+                midi_output = MixolydianTable[Math.round(value)];
                 break;
+
+            case '5':
+                var value = data / (127/75);
+                if(value > 74){
+                    value = 74;
+                }
+                midi_output = AeolianTable[Math.round(value)];
+                break;
+                
+            case '6':
+                var value = data / (127/75);
+                if(value > 74){
+                    value = 74;
+                }
+                midi_output = LocrianTable[Math.round(value)];
+                break;
+
+            case '7':
+                var value = data / (127/63);
+                if(value > 63){
+                    value = 62;
+                }
+                midi_output = WholetoneTable[Math.round(value)];
+                break;
+
+            case '8':
+                var value = data / (127/10);
+                if(value > 10){
+                    value = 9;
+                }
+                midi_output = octaveTable[Math.round(value)];
+                break;
+
+            case '9':
+                var value = data / (127/82);
+                if(value > 82){
+                    value = 81;
+                }
+                midi_output = Octatonic2_1Table[Math.round(value)];
+                break;
+
+            case '10':
+                var value = data / (127/82);
+                if(value > 82){
+                    value = 81;
+                }
+                midi_output = Octatonic1_2Table[Math.round(value)];
+                break;
+
+            case '11':
+                var value = data / (127 / 53);
+                if (value > 53) {
+                    value = 52;
+                }
+                midi_output = majPentaTable[Math.round(value)];   
+                break;
+
+            case '12':
+                var value = data / (127/53);
+                if(value > 53){
+                    value = 52;
+                }
+                midi_output = minPentaTable[Math.round(value)];
+                break;
+
+            case '13':
+                var value = data / (127/31);
+                if(value > 31){
+                    value = 30;
+                }
+                midi_output = maj3rdTable[Math.round(value)];
+                break;
+
+            case '14':
+                var value = data / (127/42);
+                if(value > 42){
+                    value = 41;
+                }
+                midi_output = min3rdTable[Math.round(value)];
+                break;
+
+            case '15':
+                var value = data / (127/53);
+                if(value > 53){
+                    value = 52;
+                }
+                midi_output = RyukyuTable[Math.round(value)];
+                break;
+
+            case '16':
+                var value = data / (127/125);
+                if(value > 125){
+                    value = 124;
+                }
+                midi_output = ChromaTable[Math.round(value)];
+                break;
+
+            default:break;
         }
         
-        return result;
+        return Math.round(midi_output);
     };
 
     oneshotGreater(args){
@@ -545,7 +902,6 @@ class ExtensionBlocks {
 
     map(args){
         var mapped = (args.data - args.in_min) * (args.out_max - args.out_min) / (args.in_max - args.in_min) + args.out_min;
-        console.log(mapped);
         return mapped;
     };
 
